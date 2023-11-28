ee1b8ad1f8939cb2b50b09cbdc716f1e68babc3b
TayiebK <150594024+mtk2608@users.noreply.github.com>
2023-11-28 13:42:28 +0200

    Update Program.cs

--- ClassExercises/Program.cs
+++ ClassExercises/Program.cs
@@ -5,10 +5,10 @@
         static void Main()
         {
             
-                double number1, number2, number3, number4, number5;
+                double number13, number2, number3, number4, number5;
 
                 Console.Write("Enter the First number: ");
-                number1 = Convert.ToDouble(Console.ReadLine());
+                number13 = Convert.ToDouble(Console.ReadLine());
 
                 Console.Write("Enter the Second number: ");
                 number2 = Convert.ToDouble(Console.ReadLine());
@@ -22,9 +22,9 @@
                 Console.Write("Enter the fifth number: ");
                 number5 = Convert.ToDouble(Console.ReadLine());
 
-                double result = (number1 + number2 + number3 + number4 + number5) / 5;
+                double result = (number13 + number2 + number3 + number4 + number5) / 5;
             Console.WriteLine("The average of {0}, {1}, {2}, {3}, {4} is: {5}",
-            number1, number2, number3, number4, number5, result);
+            number13, number2, number3, number4, number5, result);
             
 
         }
