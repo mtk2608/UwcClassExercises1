606f4fc0ce3c84c03ba28a28f52238eb627204ad
TayiebK <150594024+mtk2608@users.noreply.github.com>
2023-11-28 12:02:29 +0200

    First Class Exercises

---
+++ ClassExercises/OddNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExercises
+{
+    internal class OddNumbers
+    {
+        public static bool CheckOddNumbers(int[] numbers)
+
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int number = numbers[i];
+
+                if (number % 2 == 0)
+                {
+                    return true;
+                }
+                else
+                if (number % 2 == 1)
+                { return false; }
+            }
+
+            return false;
+        }
+
+
+    }
+}
+
---
+++ ClassExercises/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ClassExercises
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+                double number1, number2, number3, number4, number5;
+
+                Console.Write("Enter the First number: ");
+                number1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter the Second number: ");
+                number2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter the third number: ");
+                number3 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter the fourth number: ");
+                number4 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter the fifth number: ");
+                number5 = Convert.ToDouble(Console.ReadLine());
+
+                double result = (number1 + number2 + number3 + number4 + number5) / 5;
+            Console.WriteLine("The average of {0}, {1}, {2}, {3}, {4} is: {5}",
+            number1, number2, number3, number4, number5, result);
+            
+
+        }
+    }
+}
